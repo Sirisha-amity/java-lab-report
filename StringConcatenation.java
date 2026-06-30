6c19545e0b512f37da956b413ace95f359605616
Sirisha <ketha.sirisha@s.amity.edu>
2026-06-30 22:41:42 +0530

    Added StringConcatenation.java

---
+++ StringConcatenation.java
@@ -0,0 +1,10 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String firstName = "Sirisha";
+        String lastName = "Ketha";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println("Full Name: " + fullName);
+    }
+}
